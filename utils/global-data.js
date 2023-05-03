23223a87a6c91d61e182bbb34c1e6fb3ca6b7e44
Nya Ξlimu <info@elimu.ai>
2023-05-03 23:30:44 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Ξlimu DAO';
+    : 'elimu.ai';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Personalized learning for every child ✨🚀';
+    : 'Free personalized learning for every child ✨🚀';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'The purpose of elimu.ai is to provide disadvantaged children with access to quality basic education.';
